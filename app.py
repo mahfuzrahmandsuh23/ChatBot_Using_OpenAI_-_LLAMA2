41212c8bc4d3bd642796add02dc1a9f19cbbdef1
mahfuzrahmandsuh23 <126576634+mahfuzrahmandsuh23@users.noreply.github.com>
2024-04-15 00:38:59 +0100

    Add files via upload
    This project focuses on developing an advanced chatbot by leveraging the cutting-edge capabilities of OpenAI's Large Language Models (LLMs) and LLAMA2 from Ollama. The integration of these powerful AI technologies allows the chatbot to deliver highly intelligent and contextually aware interactions. OpenAI's LLMs provide a robust framework for understanding and generating human-like text, enhancing the chatbot's ability to handle diverse conversational topics

---
+++ app.py
@@ -0,0 +1,36 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st 
+import os 
+from dotenv import load_dotenv
+
+os.environ["OPENAI_API_KEY"]=os.getenv["OPENAI_API_KEY"]
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+#prompt template
+
+prompt=ChatPromptTemplate.from_messages(
+
+    [
+        ("system","You are a helpful assistant.please response to the user queries")
+        ("user","Question:{question}")
+
+    ]
+)
+
+# streamlit framework
+
+st.title('Personal ChatBot Using Langchain')
+input_text=st.text_input("search the content what you prefer")
+
+#OpenAI LLM
+llm=ChatOpenAI(model='gpt-3.5-turbo')
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
